0eb8e8bce4528c7f559b3683d77f03427ce724d1
chaelin12 <151712126+chaelin12@users.noreply.github.com>
2024-07-07 14:18:21 +0900

    product, server 커밋

--- db_setup.js
+++ db_setup.js
@@ -1,43 +1,41 @@
 const dotenv = require('dotenv').config();
-const {MongoClient} = require('mongodb');
+
+const { MongoClient } = require('mongodb');
 const mysql = require('mysql2');
 
 let mongodb;
 let mysqldb;
 
-const setup = async () => {
-  if(mongodb && mysqldb) {
-    return{mongodb, mysqldb};
-  }
+const setup = async () => { 
+    if (mongodb && mysqldb) {
+        return { mongodb, mysqldb };
+    }
 
-  try{
-    //mongodb 접속
-  const mongoDbUrl = 'mongodb+srv://admin:1234@cluster0.vgyrq7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-  const mongoConn = await MongoClient.connect(mongoDbUrl, 
-    {
-      useNewUrlParser:true,
-      useUnifiedTopology:true
-    });
-    mongodb = mongoConn.db('myboard');
-    console.log("몽고DB 접속 성공")
+    try {
+        const mongoDbUrl = process.env.MONGO_DB_URL;
+        const mongoConn = await MongoClient.connect(mongoDbUrl,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+        mongodb = mongoConn.db(process.env.MONGO_DB);
+        console.log("몽고DB 접속 성공");
 
-    //MySQL 접속
-    mysqldb = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '0719',
-      database: 'myboard'
-    });
-    mysqldb.connect();
-    console.log("MySQL 접속 성공");
+        mysqldb = mysql.createConnection({
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DB
+        });
+        mysqldb.connect();
+        console.log("MySQL 접속 성공");
 
-    return{mongodb, mysqldb};
-  } catch (err) {
-    console.error("DB 접속 실패", err);
-    //서버 가능이 안되게 한다.
-    throw err;
-  }
+        return { mongodb, mysqldb };
+    } catch (err) {
+        console.error("DB 접속 실패", err);   
+        throw err;
+    }
 };
 
-//function 코드 방출
+
 module.exports = setup;
